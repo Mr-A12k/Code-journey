689a6e93c69b966a8e14f57816f7fba4725e226e
Mr-A12k <kabil448446@gmail.com>
2025-02-13 20:29:10 +0530

    Hashmap

---
+++ HashMap/LevelOrderTraversal.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+class TreeNode{
+    int val;
+    TreeNode left, right;
+    TreeNode(int x){
+        val = x;
+    }
+}
+
+public class LevelOrderTraversal {
+
+    
+    
+}
